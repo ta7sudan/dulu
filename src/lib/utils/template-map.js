f1782901282e80199516b038a1e1e632b9716a8d
ta7sudan <ta7sudan@gmail.com>
2018-10-29 20:48:47 +0800

    update template

--- src/lib/utils/template-map.js
+++ src/lib/utils/template-map.js
@@ -3,7 +3,8 @@
 const templateMap = {
 	'multicmd-cli': 'github:ta7sudan/multicmd-cli-template',
 	'singlecmd-cli': 'github:ta7sudan/singlecmd-cli-template',
-	'lib': 'github:ta7sudan/lib-template'
+	'lib': 'github:ta7sudan/lib-template',
+	'node-lib': 'github:ta7sudan/node-lib-template'
 };
 
 module.exports = templateMap;

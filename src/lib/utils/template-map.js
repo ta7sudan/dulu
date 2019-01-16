5e286de3dc4c74db305b63921bdc7f0935f2305b
ta7sudan <ta7sudan@gmail.com>
2019-01-16 22:41:51 +0800

    add new template

--- src/lib/utils/template-map.js
+++ src/lib/utils/template-map.js
@@ -4,7 +4,8 @@ const templateMap = {
 	'multicmd-cli': 'github:ta7sudan/multicmd-cli-template',
 	'singlecmd-cli': 'github:ta7sudan/singlecmd-cli-template',
 	'lib': 'github:ta7sudan/lib-template',
-	'node-lib': 'github:ta7sudan/node-lib-template'
+	'node-lib': 'github:ta7sudan/node-lib-template',
+	'vue-mobile': 'github:ta7sudan/vue-mobile-template'
 };
 
 module.exports = templateMap;

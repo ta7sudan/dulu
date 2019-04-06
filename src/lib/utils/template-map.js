6737293c41dcd17a852424bcc484f9b7f69f5caa
ta7sudan <ta7sudan@gmail.com>
2019-04-07 01:53:58 +0800

    update template

--- src/lib/utils/template-map.js
+++ src/lib/utils/template-map.js
@@ -1,12 +1,13 @@
 'use strict';
 
 const templateMap = {
-	'multicmd-cli': 'github:ta7sudan/multicmd-cli-template',
-	'singlecmd-cli': 'github:ta7sudan/singlecmd-cli-template',
+	'multicmd': 'github:ta7sudan/multicmd-cli-template',
+	'singlecmd': 'github:ta7sudan/singlecmd-cli-template',
 	'lib': 'github:ta7sudan/lib-template',
 	'node-lib': 'github:ta7sudan/node-lib-template',
 	'ts-lib': 'github:ta7sudan/ts-lib-template',
 	'ts-node-lib': 'github:ta7sudan/ts-node-lib-template',
+	'ts-multicmd': 'github:ta7sudan/ts-multicmd-template',
 	'vue-mobile': 'github:ta7sudan/vue-mobile-template'
 };
 
